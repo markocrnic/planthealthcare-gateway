81f4847005b57fcf44300d3a6db1dd40303a9d72
markocrnic <markons996@gmail.com>
2019-09-25 15:32:12 +0200

    Redirection logic finished. v1.0

---
+++ app/api_management.py
@@ -0,0 +1,7 @@
+def paths():
+    PATH_DICT = {
+        'users': 'http://10.0.200.68:5000/',
+        'flowers': 'http://10.0.200.68:5001/',
+        'users2flowers': 'http://10.0.200.68:5002/'
+    }
+    return PATH_DICT
---
+++ app/interface.py
@@ -0,0 +1,33 @@
+from flask import Flask, redirect, request
+from flask_cors import CORS
+from api_management import paths
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/api/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def gateway(path):
+
+    redirect_path = setpath(path)
+    if redirect_path == '404':
+        return 'Path not found', 404
+    else:
+        print('Requested path: ' + redirect_path + ' | Requested method: ' + request.method)
+        return redirect(redirect_path, code=307)
+
+
+def setpath(path):
+    path_dict = paths()
+    apiname = path.split('/')[0]
+    if apiname in path_dict:
+        apipath = path_dict[apiname] + path
+        return apipath
+    else:
+        print('Path not found: ' + path)
+        return '404'
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
