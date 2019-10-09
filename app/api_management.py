57159302273e8b84bf05ecf73a037609096f4908
markocrnic <markons996@gmail.com>
2019-10-09 15:57:44 +0200

    Updated path for user management

--- app/api_management.py
+++ app/api_management.py
@@ -2,6 +2,7 @@ def paths():
     PATH_DICT = {
         'users': 'http://10.0.200.68:5000/',
         'flowers': 'http://10.0.200.68:5001/',
-        'users2flowers': 'http://10.0.200.68:5002/'
+        'users2flowers': 'http://10.0.200.68:5002/',
+        'usermanagement': 'http://10.0.200.68:5006/'
     }
     return PATH_DICT
