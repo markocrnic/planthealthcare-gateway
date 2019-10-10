84dd2dcf886615317adbaec8fd8488f06f196d44
markocrnic <markons996@gmail.com>
2019-10-10 14:02:09 +0200

    Added JWT Authorization in gateway.

--- app/interface.py
+++ app/interface.py
@@ -1,6 +1,7 @@
 from flask import Flask, redirect, request
 from flask_cors import CORS
 from api_management import paths
+import jwt
 
 
 app = Flask(__name__)
@@ -13,8 +14,19 @@ def gateway(path):
     redirect_path = setpath(path)
     if redirect_path == '404':
         return {'msg': 'Path not found'}, 404
+    elif 'usermanagement' in redirect_path:
+        return redirect(redirect_path, code=307)
     else:
         print('Requested path: ' + redirect_path + ' | Requested method: ' + request.method)
+        data = checkTokenValiditi(request)
+
+        if data == 401:
+            return {'msg': 'JWT has expired.'}, 401
+        elif data == 400:
+            return {'msg': 'Invalid JWT token'}, 401
+        elif data == 403:
+            return {'msg': 'Authorization header not present'}, 403
+
         return redirect(redirect_path, code=307)
 
 
@@ -27,6 +39,29 @@ def setpath(path):
     else:
         print('Path not found: ' + path)
         return '404'
+
+
+def checkTokenValiditi(request):
+    secret = 'planthealthcare'
+    try:
+        authorization_header = request.headers.get('Authorization')
+        token = authorization_header.split('Bearer ', 1)[1]
+    except:
+        print('Authorization header not present.')
+        return 403
+
+    try:
+
+        decoded = jwt.decode(token, secret, algorithms=['HS256'])
+        return decoded
+
+    except jwt.ExpiredSignatureError:
+        print('JWT token expired')
+        return 401
+
+    except:
+        print('Invalid JWT token')
+        return 400
 
 
 if __name__ == '__main__':
